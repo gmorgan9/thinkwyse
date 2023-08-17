80499ca3b7a3978c6106e2f2e5cd20f84687e3f3
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-17 12:15:37 -0600

    update

---
+++ assets/scripts/dropdown.js
@@ -0,0 +1,45 @@
+var currentlyOpenDropdown = null;
+    
+        function toggleDropdown(id, button) {
+            var dropdownContent = document.getElementById(id);
+    
+            if (currentlyOpenDropdown && currentlyOpenDropdown === dropdownContent) {
+                // Clicking the same dropdown again to close it
+                closeDropdown();
+            } else {
+                // Close previously open dropdown if any
+                closeDropdown();
+    
+                // Open the clicked dropdown
+                dropdownContent.classList.add("show");
+                currentlyOpenDropdown = dropdownContent;
+    
+                // Toggle chevron icon
+                var chevronIcon = button.querySelector("i");
+                chevronIcon.classList.remove("bi-chevron-down");
+                chevronIcon.classList.add("bi-chevron-up");
+    
+            }
+        }
+    
+        function closeDropdown() {
+            if (currentlyOpenDropdown) {
+                currentlyOpenDropdown.classList.remove("show");
+                currentlyOpenDropdown.previousElementSibling.querySelector("i").classList.remove("bi-chevron-up");
+                currentlyOpenDropdown.previousElementSibling.querySelector("i").classList.add("bi-chevron-down");
+
+    
+                currentlyOpenDropdown = null;
+            }
+        }
+    
+        // Close dropdown when clicking away from the dropdown button or dropdown content
+        document.addEventListener("click", function (event) {
+            var isDropdownButton = event.target.classList.contains("dropbtn");
+            var isDropdownContent = event.target.classList.contains("dropdown-content");
+            if (!isDropdownButton && !isDropdownContent) {
+                closeDropdown();
+            }
+        });
+
+    
---
+++ assets/scripts/top-scroll.js
@@ -0,0 +1,14 @@
+// Get a reference to the top-container element
+const topContainer = document.querySelector('.top-container');
+
+// Add an event listener for the "scroll" event
+window.addEventListener('scroll', function() {
+    // Check if the user has scrolled down
+    if (window.scrollY > 0) {
+        // Add the "scrolled" class to the top-container
+        topContainer.classList.add('scrolled');
+    } else {
+        // Remove the "scrolled" class from the top-container
+        topContainer.classList.remove('scrolled');
+    }
+});
